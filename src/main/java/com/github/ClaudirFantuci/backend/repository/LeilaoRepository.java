2d7e150d97655d61faa7a1f5322515bb79118fc3
Claudir Fantuci Junior <cladirfantcijunior@gmail.com>
2025-07-13 15:00:57 -0300

    refactor | ajustes na organizacao dos arquivos

--- src/main/java/com/github/ClaudirFantuci/backend/repository/LeilaoRepository.java
+++ src/main/java/com/github/ClaudirFantuci/backend/repository/LeilaoRepository.java
@@ -1,8 +1,12 @@
 package com.github.ClaudirFantuci.backend.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.github.ClaudirFantuci.backend.model.Leilao;
+import com.github.ClaudirFantuci.backend.model.StatusLeilao;
 
 public interface LeilaoRepository extends JpaRepository<Leilao, Long> {
 
+    List<Leilao> findByStatus(StatusLeilao status);
 }
